3864ec4251e7c03619a447b19d2c85ffed5b6365
Phuongan96 <phuongan05021996@gmail.com>
2022-03-15 18:59:13 +0700

    PhuongAnPart15.03

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -30,3 +30,36 @@ closeVideo.onclick = function () {
     // video_current.pause()
     console.log(234235)
 }
+
+const closeVideo_end = document.querySelector('#end .close__btn i')
+var id_end = document.getElementById("end");
+closeVideo_end.onclick = function () {
+    id_end.style.display = 'none'
+}
+
+// window.onscroll = function () {
+//     console.log(document.body.offsetHeight)
+//     window.scrollY +window.innerHeight < document.body.offsetHeight ? id_end.classList.add('p-fixed') :
+//         id_end.classList.remove('p-fixed')
+// }
+const chieudaibody = document.body.offsetHeight
+window.onscroll = function (ev) {
+    console.log("0. scroll Y: " + window.scrollY);
+    console.log("1. scroll height: " + document.body.scrollHeight);
+    console.log("2. offset height: " + document.body.offsetHeight);
+    console.log("3. client height: " + document.documentElement.clientHeight);
+    console.log("4. element height: " + document.documentElement.scrollHeight);
+    console.log("5. element offsetheight: " + document.documentElement.offsetHeight);;
+    if (window.scrollY+608>(document.body.offsetHeight - 400 )) {
+        id_end.classList.remove("p-fixed");
+    } else if(window.scrollY+608<(document.body.offsetHeight - 400 )) {
+        id_end.classList.add("p-fixed");
+    };
+}
+// window.onscroll = function(){
+// console.log("1. scroll height: "+document.body.scrollHeight);
+// console.log("2. offset height: "+document.body.offsetHeight);
+// console.log("3. client height: "+document.documentElement.clientHeight);
+// console.log("4. element height: "+document.documentElement.scrollHeight);
+//     console.log("5. element offsetheight: "+document.documentElement.offsetHeight);
+// }
